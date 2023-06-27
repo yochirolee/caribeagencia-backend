5cb39bc4c2af3b28ebb3f07cba3debca8b6dc6cf
yochirolee <yleecruz@gmail.com>
2023-06-27 14:51:29 -0400

    getSErvices

--- src/api/v1/Routes/ServicesRoutes.ts
+++ src/api/v1/Routes/ServicesRoutes.ts
@@ -1,11 +1,8 @@
 import express from "express";
-import {
-getAllServices,
-getServicesByAgencyId,
-} from "../Controllers/ServicesController";
+import { getAllServices, getServicesByAgencyId } from "../Controllers/ServicesController";
 
 const router = express.Router();
 router.get("/", getAllServices);
-router.get("/agency/:id", getServicesByAgencyId);
+router.get("/getByAgencyId/:id", getServicesByAgencyId);
 
 export default router;
